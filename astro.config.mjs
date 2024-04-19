71fb192430928bb70054b9b78cdb2f6cc7dee551
Franco <francomurabito@outlook.com>
2024-04-18 22:34:15 -0300

    added web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import paraglide from "@inlang/paraglide-js-adapter-astro";
+import vercel from "@vercel/analytics"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), react(), paraglide({
     project: "./project.inlang",
     outdir: "./src/paraglide"
